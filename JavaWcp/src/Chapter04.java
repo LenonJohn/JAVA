1e14e8cc00b1f67dc109d9d72f644a10b2c9555a
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-07-16 15:06:23 +0900

    JavaPractice210716

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+	
+	public static void main(String[] args) {
+		String str1 = "粕谷蓮音";
+		final int LUCKY_NUMBER = 7;
+		
+		System.out.println(str1);
+		
+		str1 = "不倶戴天";
+		
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+
+}
---
+++ JavaWcp/src/Variable02.java
@@ -0,0 +1,12 @@
+
+public class Variable02 {
+	
+	public static void main(String[] args) {
+//		変数の定義
+		final int MIN_NUMBER = 10;
+		System.out.println(MIN_NUMBER);
+		
+		// 再代入しようとするとエラー
+        //MIN_NUMBER = 20;
+	}
+}
