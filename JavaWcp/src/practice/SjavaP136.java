92a5e44cd758f6eb9f0c66a01d5b904ec1275d40
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-08-01 16:25:17 +0900

    JavaPractice210801

---
+++ JavaWcp/src/practice/SjavaP136.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class SjavaP136 {
+	public static void main(String[] args) {
+		boolean tenki = false;
+		if (tenki == true) {
+			System.out.println("洗濯をします");
+			System.out.println("散歩に行きます");
+		} else {
+			System.out.println("DVDを見ます");
+			System.out.println("寝ます");
+		}
+	}
+}
