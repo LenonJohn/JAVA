3e4eca9578768d81a492c99bee01d0623ec8b365
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-08-25 15:14:08 +0900

    JavaPractice210825

---
+++ JavaPractice/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "kasuya";
+		System.out.println(name);
+		
+		name = "test";
+		System.out.println(name);
+		
+		int num = 8;
+		System.out.println(num);
+	}
+}
---
+++ JavaPractice/src/chapter6.java
@@ -0,0 +1,15 @@
+
+public class chapter6 {
+	public static void main(String[] args) {
+		int a = 5;
+		int c;
+		
+		c = ++a;
+		System.out.println(c);
+		System.out.println(a);
+		
+		c = a++;
+		System.out.println(c);
+		System.out.println(a);
+	}
+}
