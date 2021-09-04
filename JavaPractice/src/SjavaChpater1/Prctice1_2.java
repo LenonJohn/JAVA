0a29fb9f11c1339370bd40cf5e86794ca0d496f9
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-09-04 16:20:47 +0900

    JavaPractice210904

---
+++ JavaPractice/src/SjavaChpater1/Prctice1_2.java
@@ -0,0 +1,11 @@
+package SjavaChpater1;
+
+public class Prctice1_2 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		
+		System.out.println("３ｘ５の答えは" + c);
+	}
+}
---
+++ JavaPractice/src/SjavaChpater1/prctice1_3.java
@@ -0,0 +1,7 @@
+package SjavaChpater1;
+
+public class prctice1_3 {
+	public static void main(String[] args) {
+		
+	}
+}
